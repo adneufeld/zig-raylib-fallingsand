a15edc794c1a5f1af71e24d6f97ce8373063f346
Andrew Neufeld <andrew.neuf@gmail.com>
2024-12-31 11:35:50 -0500

    simple density cell swap simulation

--- src/sim.zig
+++ src/sim.zig
@@ -24,7 +24,15 @@ pub const CellType = enum(u8) {
 
     pub fn freq(self: CellType) u64 {
         return switch (self) {
-            else => 100 * std.time.ns_per_ms,
+            else => 10 * std.time.ns_per_ms,
+        };
+    }
+
+    pub fn density(self: CellType) u8 {
+        return switch (self) {
+            CellType.none => 255,
+            CellType.water => 150,
+            CellType.sand => 200,
         };
     }
 };
@@ -50,15 +58,22 @@ pub fn CellularAutomata(comptime S: type) type {
                 const hInd: usize = @intCast(h);
 
                 // PROBLEM: anything moving towards the right has the "left" cell simulated moving rightwards
-                // then the right cell is simulated and the first target is leftware so it moves right back. Also
+                // then the right cell is simulated and the first target is leftward so it moves right back. Also
                 // because the update order is left to right only the right-most cell in a line of potentially
                 // right-moving cells ever updates.
 
                 for (0..self.state.mapWidth) |wInd| {
                     const cell = self.state.map[hInd][wInd];
 
-                    if (!simThisTick.get(cell.type) or cell.dirty) continue;
+                    // simulate density
+                    if (self.insideMap(wInd, hInd + 1) and
+                        cell.type.density() > self.state.map[hInd + 1][wInd].type.density())
+                    {
+                        self.swapCell(wInd, hInd, wInd, hInd + 1);
+                    }
 
+                    // simulate cell types
+                    if (!simThisTick.get(cell.type) or cell.dirty) continue;
                     switch (cell.type) {
                         CellType.sand => self.sand(wInd, hInd),
                         CellType.water => self.water(wInd, hInd),
@@ -93,6 +108,31 @@ pub fn CellularAutomata(comptime S: type) type {
                 y >= 0 and y <= self.state.mapHeight - 1;
         }
 
+        // swap two cells with non-none types
+        fn swapCell(self: *Self, x1: usize, y1: usize, x2: usize, y2: usize) void {
+            if (!self.insideMap(x1, y1) or self.state.map[y1][x1].type == CellType.none) return;
+            if (!self.insideMap(x2, y2) or self.state.map[y2][x2].type == CellType.none) return;
+
+            const topCell = self.state.map[y1][x1];
+            self.state.map[y1][x1] = self.state.map[y2][x2];
+            self.state.map[y2][x2] = topCell;
+
+            self.state.map[y1][x1].dirty = true;
+            self.state.map[y2][x1].dirty = true;
+        }
+
+        // move a non-none cell into an empty (none) cell
+        fn moveCell(self: *Self, x1: usize, y1: usize, x2: usize, y2: usize) void {
+            if (self.state.map[y2][x2].type != CellType.none) {
+                @panic("cannot moveCell into a non-none cell");
+            }
+
+            self.state.map[y2][x2] = self.state.map[y1][x1];
+            self.state.map[y2][x2].dirty = true;
+            self.state.map[y1][x1].type = CellType.none;
+            self.state.map[y1][x1].dirty = false;
+        }
+
         fn sand(self: *Self, x: usize, y: usize) void {
             const targets = [3]struct { x: usize, y: usize }{
                 .{ .x = x, .y = y + 1 }, // down
@@ -102,11 +142,18 @@ pub fn CellularAutomata(comptime S: type) type {
 
             for (targets) |t| {
                 if (!self.insideMap(t.x, t.y)) continue;
-                if (self.state.map[t.y][t.x].type != CellType.none) continue;
 
-                self.state.map[y][x].type = CellType.none;
-                self.state.map[t.y][t.x].type = CellType.sand;
-                self.state.map[t.y][t.x].dirty = true;
+                const targetHasCell = self.state.map[t.y][t.x].type != CellType.none;
+                const targetIsLowerDensity = CellType.sand.density() > self.state.map[t.y][t.x].type.density();
+
+                if (targetHasCell and !targetIsLowerDensity) continue;
+
+                if (targetHasCell and targetIsLowerDensity) {
+                    self.swapCell(x, y, t.x, t.y);
+                } else {
+                    self.moveCell(x, y, t.x, t.y);
+                }
+
                 return;
             }
         }
@@ -122,11 +169,18 @@ pub fn CellularAutomata(comptime S: type) type {
 
             for (targets) |t| {
                 if (!self.insideMap(t.x, t.y)) continue;
-                if (self.state.map[t.y][t.x].type != CellType.none) continue;
 
-                self.state.map[y][x].type = CellType.none;
-                self.state.map[t.y][t.x].type = CellType.water;
-                self.state.map[t.y][t.x].dirty = true;
+                const targetHasCell = self.state.map[t.y][t.x].type != CellType.none;
+                const targetIsLowerDensity = CellType.water.density() > self.state.map[t.y][t.x].type.density();
+
+                if (targetHasCell and !targetIsLowerDensity) continue;
+
+                if (targetHasCell and targetIsLowerDensity) {
+                    self.swapCell(x, y, t.x, t.y);
+                } else {
+                    self.moveCell(x, y, t.x, t.y);
+                }
+
                 return;
             }
 
